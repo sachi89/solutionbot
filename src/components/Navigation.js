9700fa5e84f4241bb949bfcf503e18ffd40ca1f3
Sachi <sachini.tissera@gmail.com>
2025-02-07 13:52:18 -0500

    fixed image errors

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -11,15 +11,15 @@ function Navigation() {
         <div>
             <Navbar expand="lg"> {/* navbar and nav from react-bootstrap */}
             {/* SolutionBot logo and link to homepage */}
-            <Navbar.Brand href="/"><img src="/images/logo.png" alt="logo" width="350" /></Navbar.Brand>
+            <Navbar.Brand href="/"><img src="../images/logo.png" alt="logo" width="350" /></Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
               <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="navigation">
               {/*Link component from react-router-dom used to route to different components*/}
                 {/* links to Chatbox component */}
-                <Nav.Link> <Link to={"/"}><img src="/images/chat.png" alt="chat" width="72" className="grow" /></Link></Nav.Link>
+                <Nav.Link> <Link to={"/"}><img src="../images/chat.png" alt="chat" width="72" className="grow" /></Link></Nav.Link>
                 {/* links to About component */}
-                <Nav.Link> <Link to={"/About"}><img src="/images/about.png" alt="about" width="94" className="grow" /></Link></Nav.Link>
+                <Nav.Link> <Link to={"/About"}><img src="../images/about.png" alt="about" width="94" className="grow" /></Link></Nav.Link>
               </Nav>
             </Navbar.Collapse>
           </Navbar>
